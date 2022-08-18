cc4514296dd6358ed2c53838995b9a4960883689
Matheus Fernandes <matheus.mendescf@gmail.com>
2022-08-18 19:38:45 -0300

    Remove mocked tracking ids

--- gatsby-config.js
+++ gatsby-config.js
@@ -145,8 +145,6 @@ module.exports = {
         // You can add multiple tracking ids and a pageview event will be fired for all of them.
         trackingIds: [
           process.env.GOOGLE_ANALYTICS_TRACKING_ID, // Google Analytics / GA
-          "AW-CONVERSION_ID", // Google Ads / Adwords / AW
-          "DC-FLOODIGHT_ID", // Marketing Platform advertising products (Display & Video 360, Search Ads 360, and Campaign Manager)
         ],
         // This object is used for configuration specific to this plugin
         pluginConfig: {
